9fa4c4f5549ee6b37297a3ae94a4a6c666c13ba4
tawashley <trisashley@gmail.com>
2016-06-07 15:21:57 +0100

    feat: Add string template tests

--- koans/AboutStrings.js
+++ koans/AboutStrings.js
@@ -10,11 +10,24 @@ describe("About Strings", function () {
     });
 
     it("should understand function substitution", function() {
+      function typeOfSituation(goodOrBad) {
+        if(goodOrBad === 'bad') {
+          return 'trap!';
+        }
 
+        return 'good day!'
+      }
+
+      var templateFunction = `It's a ${typeOfSituation('bad')}`
+
+      expect(templateFunction).toEqual(FILL_ME_IN);
     })
 
     it('should understand template expressions', function() {
+      var wrongParsecs = 10;
+      var templateExpression = `...Kessel Run in ${wrongParsecs + 2} parsecs? ${wrongParsecs + 4}!`;
 
+      expect(templateExpression).toEqual(FILL_ME_IN);
     })
   });
 
