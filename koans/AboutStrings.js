bf168f0d1c25f6515ae2ce17720f4e7a388c86d8
tawashley <trisashley@gmail.com>
2016-06-07 16:07:14 +0100

    feat: Add new string methods tests

--- koans/AboutStrings.js
+++ koans/AboutStrings.js
@@ -32,9 +32,43 @@ describe("About Strings", function () {
   });
 
   describe("New String Methods", function() {
-    //.startsWith
-    //.endsWith
-    //.includes
-    //.repeat
+
+    it('should understand startsWith function', function() {
+      var string = 'Tower Bridge';
+
+      expect(string.startsWith('Tow')).toEqual(FILL_ME_IN);
+      expect(string.startsWith('rid')).toEqual(FILL_ME_IN);
+      expect(string.startsWith('Bridge', 6)).toEqual(FILL_ME_IN);
+      expect(string.startsWith('bridge', 6)).toEqual(FILL_ME_IN);
+    })
+
+    it('should understand endsWith function', function() {
+      var string = 'Tower Bridge';
+
+      expect(string.endsWith('Tow')).toEqual(FILL_ME_IN);
+      expect(string.endsWith('Bridge')).toEqual(FILL_ME_IN);
+      expect(string.endsWith('Tower', 5)).toEqual(FILL_ME_IN);
+      expect(string.endsWith('tower', 5)).toEqual(FILL_ME_IN);
+    })
+
+    it('should understand includes function', function() {
+      var string = 'Tower Bridge';
+
+      expect(string.includes('Tow')).toEqual(FILL_ME_IN);
+      expect(string.includes('rid')).toEqual(FILL_ME_IN);
+      expect(string.includes('bridge')).toEqual(FILL_ME_IN);
+      expect(string.includes('Tower', 5)).toEqual(FILL_ME_IN);
+    })
+
+    it('should understand repeat function', function() {
+      var ga = ' ga';
+      var goo =' goo';
+
+      var lineOne = 'All we hear is Radio' + ga.repeat(2);
+      var lineTwo = 'Radio' + goo.repeat(2);
+
+      expect(lineOne).toEqual(FILL_ME_IN);
+      expect(lineTwo).toEqual(FILL_ME_IN);
+    })
   })
 });
