9d9e9fd64404ec618f483427881f9ed8b0ec8fe5
tawashley <trisashley@gmail.com>
2016-06-07 15:03:26 +0100

    feat: Add base structure for string koans

---
+++ koans/AboutStrings.js
@@ -0,0 +1,24 @@
+describe("About Strings", function () {
+
+  describe("Template Literals", function () {
+
+    it("should understand variable substitution", function () {
+      
+    });
+
+    it("should understand function substitution", function() {
+
+    })
+
+    it('should understand template expressions', function() {
+
+    })
+  });
+
+  describe("New String Methods", function() {
+    //.startsWith
+    //.endsWith
+    //.includes
+    //.repeat
+  })
+});
