ed96800ae4ddf1296f55152a3084df51623879db
Tristan Ashley <trisashley@gmail.com>
2016-06-07 19:31:02 +0100

    feat: Add default param expression test

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -2,7 +2,7 @@ describe("About Functions", function () {
 
   describe("Default Parameters", function () {
 
-    it("should understand how default parameters are handled", function () {
+    it("should understand default parameters basic usage", function () {
 
       function greeting(string = 'party') {
         return 'Welcome to the ' + string + ' pal!';
@@ -10,6 +10,17 @@ describe("About Functions", function () {
 
       expect(greeting()).toEqual(FILL_ME_IN);
       expect(greeting('get together')).toEqual(FILL_ME_IN);
+
+      function getDefaultValue() {
+        return 'party';
+      }
+
+      function greetingAgain(string = getDefaultValue()) {
+        return 'Welcome to the ' + string + ' pal!';
+      }
+
+      expect(greetingAgain()).toEqual(FILL_ME_IN);
+      expect(greetingAgain('get together')).toEqual(FILL_ME_IN);
     });
   });
 
