4ba9ce952cd31c51301a27cdcda61f63428414eb
tawashley <trisashley@gmail.com>
2016-06-07 16:12:28 +0100

    chore: Comment out template koans to stop it showing in the test runner

--- koans/AboutBase.js
+++ koans/AboutBase.js
@@ -1,14 +1,14 @@
-describe("About <SubjectName>", function () {
-
-  describe("<SubjectPart>", function () {
-
-    beforeEach(function () {});
-    afterEach(function () {});
-    before(function () {});
-    after(function () {});
-
-    it("should <explain what it does>", function () {
-      // expect().toBe();
-    });
-  });
-});
+// describe("About <SubjectName>", function () {
+//
+//   describe("<SubjectPart>", function () {
+//
+//     beforeEach(function () {});
+//     afterEach(function () {});
+//     before(function () {});
+//     after(function () {});
+//
+//     it("should <explain what it does>", function () {
+//       // expect().toBe();
+//     });
+//   });
+// });
