62614ccf399c8100f847518702261e4b01eeb19b
tawashley <trisashley@gmail.com>
2016-06-13 17:21:12 +0100

    feat: Set base promise koan structure

---
+++ koans/AboutPromises.js
@@ -0,0 +1,9 @@
+describe("About Promises", function () {
+
+  describe("Asynchronous Flow", function () {
+
+    it("should understand promise declaration usage", function () {
+    
+    });
+  });
+});
