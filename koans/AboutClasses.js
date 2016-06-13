d1a5a38b8b944c35fa3ce32b4fbbf5add9ea705e
tawashley <trisashley@gmail.com>
2016-06-13 16:51:14 +0100

    feat: Add tests about getters and setters

--- koans/AboutClasses.js
+++ koans/AboutClasses.js
@@ -45,6 +45,8 @@ describe("About Classes", function () {
 
   describe('Classes as First-Class Citizens', function() {
 
+    it('should understand passing classes around', function() {
+
       function constructCarClass(classObj, make, model) {
         return new classObj(make, model);
       }
@@ -60,7 +62,40 @@ describe("About Classes", function () {
           }
       }, 'Ford', 'Mustang');
 
-      expect(car.getFullName()).toEqual(FILL_ME_IN);
+      // expect(car.getFullName()).toEqual(FILL_ME_IN);
+    })
+  })
+
+  describe('Getters and Setters', function() {
+
+    it('should understand syntax for getters and setters', function() {
+      class Car2 {
+          constructor() {
+            this._make = undefined;
+          }
+
+          get make() {
+            if(typeof this._make === 'undefined') {
+              return 'No car set';
+            }
+
+            return this._make;
+          }
+          set make(newMake) {
+              this._make = newMake;
+          }
+      }
+
+      var car = new Car2();
+
+      // expect(car.make).toEqual(FILL_ME_IN);
+
+      car.make = 'Ford';
+      // expect(car.make).toEqual(FILL_ME_IN);
+
+      car.make = 'Bugatti';
+      // expect(car.make).toEqual(FILL_ME_IN);
+    })
   })
 
 });
