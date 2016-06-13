b56cc9ae12940abd1ee5632b9576344106f05048
tawashley <trisashley@gmail.com>
2016-06-13 15:12:42 +0100

    feat: Add base structure for arrays koans

---
+++ koans/AboutArrays.js
@@ -0,0 +1,38 @@
+describe("About Arrays", function () {
+
+  describe("New Prototype Methods", function () {
+
+    it("should understand Array.of", function () {
+      var zeroToTen = Array.of(0,1,2,3,4,5,6,7,8,9,10);
+
+      expect(zeroToTen[0]).toEqual(FILL_ME_IN);
+      expect(zeroToTen[5]).toEqual(FILL_ME_IN);
+      expect(zeroToTen[10]).toEqual(FILL_ME_IN);
+    });
+
+    it('should understand Array.from', function() {
+
+    })
+
+  });
+
+  describe('New Instance Methods', function() {
+
+    it('should understand array.find', function() {
+
+    })
+
+    it('should understand array.findIndex', function() {
+
+    })
+
+    it('should understand array.fill', function() {
+
+    })
+
+    it('should understand array.copyWithin', function() {
+
+    })
+
+  })
+});
