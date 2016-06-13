8599b9c74d4273843cb5ae4f11a5799c2f1f8cc5
tawashley <trisashley@gmail.com>
2016-06-13 17:28:09 +0100

    feat: Add type test for promises

--- koans/AboutPromises.js
+++ koans/AboutPromises.js
@@ -2,8 +2,20 @@ describe("About Promises", function () {
 
   describe("Asynchronous Flow", function () {
 
-    it("should understand promise declaration usage", function () {
-    
+    it("should understand promise usage", function () {
+
+      function isZero(number) {
+        return new Promise(function(resolve, reject) {
+          if(number === 0) {
+            resolve();
+          } else {
+            reject(number + ' is not zero!');
+          }
+        })
+      }
+
+      expect(isZero(0) instanceof Promise).toEqual(FILL_ME_IN);
+      expect(typeof isZero(0)).toEqual(FILL_ME_IN);
     });
   });
 });
