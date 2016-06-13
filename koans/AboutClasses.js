aca55a4941947799f5095d4882f042a59e4c2f88
tawashley <trisashley@gmail.com>
2016-06-13 16:23:31 +0100

    feat: Add in first-class citizen test

--- koans/AboutClasses.js
+++ koans/AboutClasses.js
@@ -18,11 +18,11 @@ describe("About Classes", function () {
 
       let car = new Car('Ford', 'Mustang');
 
-      expect(car instanceof Car).toEqual(FILL_ME_IN);
-      expect(car instanceof Object).toEqual(FILL_ME_IN);
-
-      expect(typeof car.getFullName).toEqual(FILL_ME_IN);
-      expect(car.getFullName()).toEqual(FILL_ME_IN);
+      // expect(car instanceof Car).toEqual(FILL_ME_IN);
+      // expect(car instanceof Object).toEqual(FILL_ME_IN);
+      //
+      // expect(typeof car.getFullName).toEqual(FILL_ME_IN);
+      // expect(car.getFullName()).toEqual(FILL_ME_IN);
 
       let Bike = class {
         constructor(make, model) {
@@ -37,9 +37,30 @@ describe("About Classes", function () {
 
       let bike = new Bike('Ducati', 'Monster');
 
-      expect(typeof bike.getFullName).toEqual(FILL_ME_IN);
-      expect(bike.getFullName()).toEqual(FILL_ME_IN);
+      // expect(typeof bike.getFullName).toEqual(FILL_ME_IN);
+      // expect(bike.getFullName()).toEqual(FILL_ME_IN);
 
     });
   });
+
+  describe('Classes as First-Class Citizens', function() {
+
+      function constructCarClass(classObj, make, model) {
+        return new classObj(make, model);
+      }
+
+      var car = constructCarClass(class {
+          constructor(make, model) {
+            this.make = make;
+            this.model = model;
+          }
+
+          getFullName() {
+            return this.make + ' ' + this.model;
+          }
+      }, 'Ford', 'Mustang');
+
+      expect(car.getFullName()).toEqual(FILL_ME_IN);
+  })
+
 });
