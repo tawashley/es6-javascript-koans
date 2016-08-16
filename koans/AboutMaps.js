fcab48b4817af4bb727ac8fc6ed8506e6b88c8f1
tawashley <trisashley@gmail.com>
2016-08-16 13:58:51 +0100

    feat: Add Map initial koan

--- koans/AboutMaps.js
+++ koans/AboutMaps.js
@@ -2,20 +2,19 @@ describe("About Maps", function () {
 
   describe("Basic Usage", function () {
 
-    beforeEach(function () {});
-    afterEach(function () {});
-    before(function () {});
-    after(function () {});
+    it("should understand they are key, value stores", function () {
+      var trooper = new Map();
 
-    it("should they are key, value stores", function () {
-      var person = new Map();
       trooper.set('name', 'Stormtrooper');
       trooper.set('Droid you are looking for?', false);
       trooper.set('hits target', function() {
         return false;
       })
 
-      expect(trooper.size).toBe(FILL_ME_IN);
+      expect(trooper.size).toEqual(FILL_ME_IN);
+      expect(trooper.get('name')).toEqual(FILL_ME_IN);
+      expect(trooper.get('hits target')()).toEqual(FILL_ME_IN);
     });
+
   });
 });
