caedfe69291165ddedb0a041be3eb65a9a12442a
tawashley <trisashley@gmail.com>
2016-08-18 13:59:09 +0100

    docs: Add helper comments in generators koans

--- koans/AboutGenerators.js
+++ koans/AboutGenerators.js
@@ -2,18 +2,23 @@ describe("About Generators", function () {
 
   describe("Basic usage", function () {
 
-    it("should understand concept", function () {
+    it("should understand syntax", function () {
+
+      // `*` immediately after `function` denotes a generator
       function* createGenerator() {
         //`yield` keyword exits function execution
         yield 1;
       }
 
+      // invoking a generator function returns a generator interface
+      // `.next()` is used to `start` a generator
+      // Also used to resume function execution to the next `yield`
       var generator = createGenerator();
 
       expect(generator.next().value).toEqual(FILL_ME_IN);
 
       var completedGenerator = generator.next()
-      
+
       expect(completedGenerator.value).toEqual(FILL_ME_IN);
       expect(completedGenerator.done).toEqual(FILL_ME_IN);
 
