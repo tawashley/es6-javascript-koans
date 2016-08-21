43d771162f5993035a231857d02e97d94e74c3a6
tawashley <trisashley@gmail.com>
2016-08-21 15:35:20 +0100

    feat: Add in extra promise koan

--- koans/AboutPromises.js
+++ koans/AboutPromises.js
@@ -2,20 +2,32 @@ describe("About Promises", function () {
 
   describe("Asynchronous Flow", function () {
 
-    it("should understand promise usage", function () {
+    it("should understand Promise type", function () {
 
-      function isZero(number) {
+      function promise(number) {
         return new Promise(function(resolve, reject) {
-          if(number === 0) {
             resolve();
-          } else {
-            reject(number + ' is not zero!');
-          }
         })
       }
 
-      expect(isZero(0) instanceof Promise).toEqual(FILL_ME_IN);
-      expect(typeof isZero(0)).toEqual(FILL_ME_IN);
+      // expect(promise() instanceof Promise).toEqual(FILL_ME_IN);
+      // expect(typeof promise()).toEqual(FILL_ME_IN);
     });
+
+    it('should understand a Promise can be fulfilled / resolved', function(done) {
+
+      function promiseResolved() {
+        return new Promise(function(resolve, reject) {
+          resolve('promise me this');
+        });
+      }
+
+      promiseResolved()
+        .then(function(promiseValue) {
+          expect(promiseValue).toEqual(promiseValue);
+        })
+        .then(done)
+    })
+
   });
 });
